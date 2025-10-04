4d044ce8f908e1e34fb829919f2f38bd13f1ceaf
GeorgiApostolov <74099975+GeorgiApostolov@users.noreply.github.com>
2025-10-04 13:29:04 +0300

    Ready body data

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -7,4 +7,9 @@ movieController.get("/create", (req, res) => {
   res.render("create");
 });
 
+movieController.post("/create", (req, res) => {
+  console.log(req.body);
+  res.end();
+});
+
 export default movieController;
