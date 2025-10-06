e0c83acfe623ccb4095bdd8503eb8d81e0bb46b4
GeorgiApostolov <74099975+GeorgiApostolov@users.noreply.github.com>
2025-10-06 12:07:29 +0300

    Dynamic page title

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -5,7 +5,7 @@ import movieService from "../services/movieService.js";
 const movieController = Router();
 
 movieController.get("/create", (req, res) => {
-  res.render("create");
+  res.render("create", { pageTitle: "Movie Create" });
 });
 
 movieController.post("/create", (req, res) => {
@@ -17,7 +17,7 @@ movieController.post("/create", (req, res) => {
 movieController.get("/:movieId/details", (req, res) => {
   const movieId = req.params.movieId;
   const movie = movieService.getOne(movieId);
-  res.render("details", { movie });
+  res.render("details", { movie, pageTitle: "Movie Details" });
 });
 
 movieController.get("/search", (req, res) => {
@@ -25,7 +25,7 @@ movieController.get("/search", (req, res) => {
 
   const movies = movieService.getAll(filter);
 
-  res.render("search", { movies, filter });
+  res.render("search", { movies, filter, pageTitle: "Search Movies" });
 });
 
 export default movieController;
